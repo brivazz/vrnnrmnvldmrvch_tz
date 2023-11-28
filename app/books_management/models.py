996526921ede17d0fda210784832b03b5add7ce3
Sergey Toporets <yateperna@yandex.ru>
2023-11-28 13:04:11 +0300

    fix: Несущественные правки

--- app/books_management/models.py
+++ app/books_management/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     registration_date = models.DateTimeField(auto_now_add=True)
+    welcome_email_sent = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
--- app/books_management/tasks.py
+++ app/books_management/tasks.py
@@ -1,5 +1,7 @@
 import os
 
+import logging
+
 from dotenv import load_dotenv
 from celery import shared_task
 from django.core.mail import send_mail
@@ -8,15 +10,27 @@ from .models import UserProfile
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+
 
 @shared_task
 def send_welcome_email(user_id):
-    user = User.objects.get(id=user_id)
-    user_profile = UserProfile.objects.get(user=user)
+    try:
+        user = User.objects.get(id=user_id)
+        user_profile = UserProfile.objects.get(user=user)
 
-    subject = f'Welcome to Our Website, {user_profile.user.username}!'
-    message = f'Thank you for registering on our website. Your registration date is {user_profile.registration_date}.'
-    from_email = os.environ.get('EMAIL_HOST_USER')
-    recipient_list = [user.email]
+        if user_profile.welcome_email_sent:
+            return
 
-    send_mail(subject, message, from_email, recipient_list)
+        subject = f'Welcome to Our Website, {user_profile.user.username}!'
+        message = f'Thank you for registering. Your registration date is {user_profile.registration_date}.'
+        from_email = os.environ.get('EMAIL_HOST_USER')
+        recipient_list = [user.email]
+
+        send_mail(subject, message, from_email, recipient_list)
+
+        user_profile.welcome_email_sent = True
+        user_profile.save()
+
+    except Exception as e:
+        logger.error(f"Error sending welcome email: {str(e)}")
